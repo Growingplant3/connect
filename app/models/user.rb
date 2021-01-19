e34b2dcc7798e70df4e3e1d7af9e5eaafe12c259
Yoshinori Shimoda <ys@platinum.local>
2021-01-19 16:06:49 +0900

    Userモデルにenumを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   validates :postcode, format: { with: /\A[0-9]+\z/ }, length: { is: 7 }
   validates :telephone_number, format: { with: /\A[0-9]+\z/ }
   validates :sex, :role, format: { with: /[0-2]/ }
+  enum sex: { unknown: 0, male: 1, female: 2 }
+  enum role: { normal: 0, developer: 1, master: 2 }
 end
