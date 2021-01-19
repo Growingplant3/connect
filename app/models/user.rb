4d5df8efbec19474cc5fcf0673f4523ff18e45b1
Yoshinori Shimoda <ys@platinum.local>
2021-01-19 16:02:44 +0900

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :postcode, format: { with: /\A[0-9]+\z/ }, length: { is: 7 }
+  validates :telephone_number, format: { with: /\A[0-9]+\z/ }
+  validates :sex, :role, format: { with: /[0-2]/ }
 end
