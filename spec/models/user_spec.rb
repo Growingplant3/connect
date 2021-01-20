a73d58df8bf04552f1f43601d6d77ad78050227f
Yoshinori Shimoda <ys@platinum.local>
2021-01-20 19:33:52 +0900

    Userモデルのmodelスペックを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,30 @@ RSpec.describe 'ユーザーモデル機能', type: :model do
   let(:user) { FactoryBot.create(:user) }
   
   describe 'モデルの作成' do
-    context '新規登録' do
+    context '最低限の新規登録' do
       it '必須項目を満たせばアカウント登録できる' do
         expect(new_user).to be_valid
+      end
+    end
+
+    context '名前のない新規登録' do
+      it '名前がnilだとアカウント登録できない' do
+        new_user.name = nil
+        expect(new_user).not_to be_valid
+      end
+    end
+
+    context 'メールアドレスのない新規登録' do
+      it 'メールアドレスがnilだとアカウント登録できない' do
+        new_user.email = nil
+        expect(new_user).not_to be_valid
+      end
+    end
+
+    context 'パスワードのない新規登録' do
+      it 'パスワードがnilだとアカウント登録できない' do
+        new_user.password = nil
+        expect(new_user).not_to be_valid
       end
     end
   end
