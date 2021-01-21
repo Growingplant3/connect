c8f4001b17f74a361f937b3189f7632ac79f292b
Yoshinori Shimoda <ys@platinum.local>
2021-01-21 17:10:37 +0900

    サインアップなどのボタンヘルパー整備

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def sign_in_or_sign_out_button
+    if user_signed_in?
+      link_to "ログアウトする", destroy_user_session_path, method: :delete
+    else
+      link_to "ログインする", new_user_session_path
+    end
+  end
+
+  def sign_up_button
+    unless user_signed_in?
+      link_to "アカウントを作成する", new_user_registration_path
+    end
+  end
 end
