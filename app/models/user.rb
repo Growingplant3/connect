51e0e3b877d604e24b00fdf325c40bd40aabcdd2
Yoshinori Shimoda <ys@platinum.local>
2021-01-21 13:36:46 +0900

    バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,18 @@ class User < ApplicationRecord
   validates :telephone_number, format: { with: /\A[0-9]+\z/ }, length: { in: 10..11 }, allow_nil: true
   validates :sex, inclusion: { in: %w(unknown male female) }
   validates :role, inclusion: { in: %w(normal developer master) }
+  validate :should_be_past
   enum sex: { unknown: 0, male: 1, female: 2 }
   enum role: { normal: 0, developer: 1, master: 2 }
 
   def before_save
     self.postcode = DowncaseCallback.replace_to_half_num(self.postcode) if self.postcode.present?
     self.telephone_number = DowncaseCallback.replace_to_half_num(self.telephone_number) if self.telephone_number.present?
+  end
+
+  def should_be_past
+    if birthday.present? && birthday > Date.current
+      errors.add(:birthday, I18n.t('errors.messages.should_be_past'))
+    end
   end
 end
