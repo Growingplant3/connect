aa851b6adc451477b9e5ba6b9fd236769b06fedd
Yoshinori Shimoda <ys@platinum.local>
2021-01-22 13:47:31 +0900

    日本語訳を適応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
   def sign_in_or_sign_out_button
     if user_signed_in?
-      link_to "ログウトする", destroy_user_session_path, method: :delete
+      link_to t('button.sign_out'), destroy_user_session_path, method: :delete
     else
-      link_to "ログンする", new_user_session_path
+      link_to t('devise.sessions.new.sign_in'), new_user_session_path
     end
   end
 
   def sign_up_or_resource_show_button
     if user_signed_in?
-      link_to "ユーザ情報確認", user_path(current_user)
+      link_to t('button.user_show'), user_path(current_user)
     else
-      link_to "アカントを作成する", new_user_registration_path
+      link_to t('devise.registrations.new.sign_up'), new_user_registration_path
     end
   end
 end
