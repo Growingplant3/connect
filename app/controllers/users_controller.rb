54202126e39314093b928a2b6b04f74be6ca2db6
Yoshinori Shimoda <ys@platinum.local>
2021-01-25 12:13:33 +0900

    updateアクション整備/strong parameter整備

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,21 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "編集成功"
+      redirect_to @user
+    else
+      flash.now[:alert] = "編集失敗"
+      render :edit
+    end
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:postcode, :address, :telephone_number, :birthday, :sex, :side_effect, :allergy, :sick, :operation, :note)
+  end
 
   def set_user
     @user = User.find(params[:id])
