a62949c1dacb35eabdcc44443a0bb5f82fdf26d3
Yoshinori Shimoda <ys@platinum.local>
2021-01-26 18:31:30 +0900

    テストコード追加

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -33,7 +33,32 @@ RSpec.describe 'ユーザー管理機能', type: :system do
           click_on 'アカウントを削除する'
         end
         expect(page).to have_content 'アカウントを削除しました。またのご利用をお待ちしております。'
-      # end
-    end
+      end
+
+      it 'ユーザー情報編集ボタンからアカウントが編集できる' do
+        visit edit_user_path(new_user)
+        fill_in 'user[postcode]', with: '1234567'
+        fill_in 'user[address]', with: '神奈川'
+        fill_in 'user[telephone_number]', with: '09012345678'
+        fill_in 'user[birthday]', with: '001999-12-31'
+        find("option[value='female']").select_option
+        fill_in 'user[side_effect]', with: '特になし'
+        fill_in 'user[allergy]', with: '特になし'
+        fill_in 'user[sick]', with: '特になし'
+        fill_in 'user[operation]', with: '特になし'
+        fill_in 'user[note]', with: '特になし'
+        click_on '更新'
+        expect(page).to have_content 'アカウント情報を変更しました。' && 'ユーザー情報を確認中'
+      end
+
+      it 'パスワード更新ボタンからパスワードが再設定できる' do
+        visit edit_user_registration_path
+        fill_in 'user[password]', with: 'new_password'
+        fill_in 'user[password_confirmation]', with: 'new_password'
+        fill_in 'user[current_password]', with: 'new_user'
+        click_on '更新'
+        expect(page).to have_content 'アカウント情報を変更しました。'
+        expect(current_path).to eq root_path
+      end
   end
 end
