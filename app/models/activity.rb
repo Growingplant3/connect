2ea85e5696d8088bf89c4679d8a7bcaef76cb49d
Yoshinori Shimoda <ys@platinum.local>
2021-02-03 15:33:12 +0900

    Activityモデルのテストコード作成

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,7 @@
 class Activity < ApplicationRecord
-  belongs_to :pharmacy
+  validates :business, inclusion: { in: %w(true false) }
+  validates :day_of_the_week, inclusion: { in: %w(monday tuesday wednesday thursday friday saturday sunday) }
   enum business: { true: true, false: false }
   enum day_of_the_week: { monday: 0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 6 }
+  belongs_to :pharmacy
 end
