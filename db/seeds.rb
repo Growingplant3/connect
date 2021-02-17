826bfd15895415c42a58d00c113bba16b645ed4e
Yoshinori Shimoda <ys@platinum.local>
2021-02-17 12:28:29 +0900

    seedデータ改修/追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@ User.create(name: "菊池", email: "kikuchi@gmail.com", password: "password", po
 birthday: "1965-01-02".to_date, sex: 1, side_effect: "特に無し", allergy: "特に無し", sick: "特に無し", operation: "特に無し", note: "特に無し", role: 0)
 User.create(name: "菊川", email: "kikukiku@gmail.com", password: "password", postcode: "8100031", address: "福岡県筑後市", telephone_number: "09213579086",
 birthday: nil, sex: 0, side_effect: "特に無し", allergy: "特に無し", sick: "特に無し", operation: "特に無し", note: "特に無し", role: 0)
-User.create(name: "開発者", email: "developer@gmail.com", password: "password", role: 1)
+User.create(name: "武○製薬の開発者", email: "developer@gmail.com", password: "password", role: 1)
+User.create(name: "塩○義製薬の開発者", email: "developer2@gmail.com", password: "password", role: 1)
 User.create(name: "管理者", email: "admin@gmail.com", password: "password", role: 2)
 
 first_pharmacy = Pharmacy.create(name: "ひまわり薬局", email: "himawari@gmail.com", password: "password", postcode: "2720003", address: "千葉県市川市",
@@ -18,12 +19,12 @@ normal_telephone_number: "04711112222", emergency_telephone_number: "09012341234
 fourth_pharmacy = Pharmacy.create(name: "はな薬局", email: "hana@gmail.com", password: "password", postcode: "2730107", address: "千葉県鎌ヶ谷市",
 normal_telephone_number: "0472234499", emergency_telephone_number: "08087656543", note: "全国どこの病院の処方箋でも受け付けております")
 
-4.times do |pharmacy_identification|
+1.upto(4) do |pharmacy_identification|
   5.times do |index|
-    Activity.create(pharmacy_id: pharmacy_identification+1, day_of_the_week: index, business: true, opening_time: "09:00", closing_time: "18:00")
+    Activity.create(pharmacy_id: pharmacy_identification, day_of_the_week: index, business: true, opening_time: "09:00", closing_time: "18:00")
   end
-  Activity.create(pharmacy_id: pharmacy_identification+1, day_of_the_week: 5, business: "false", opening_time: nil, closing_time: nil)
-  Activity.create(pharmacy_id: pharmacy_identification+1, day_of_the_week: 6, business: "false", opening_time: nil, closing_time: nil)
+  Activity.create(pharmacy_id: pharmacy_identification, day_of_the_week: 5, business: "false", opening_time: nil, closing_time: nil)
+  Activity.create(pharmacy_id: pharmacy_identification, day_of_the_week: 6, business: "false", opening_time: nil, closing_time: nil)
 end
 
 1.upto(4) do |user_identification|
