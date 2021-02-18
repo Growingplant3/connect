f2b37857ab6a74ca4df49792bb39389649a8bab3
Yoshinori Shimoda <ys@platinum.local>
2021-02-18 17:27:21 +0900

    親子関係の記述/選択肢用のクラスメソッド記述

--- app/models/take_method_detail.rb
+++ app/models/take_method_detail.rb
@@ -1,3 +1,12 @@
 class TakeMethodDetail < ApplicationRecord
   validates :style, presence: true
+  has_many :take_medicine_relations
+
+  def self.to_hash_for_choices
+    hash = {}
+    self.pluck(:style).zip(self.pluck(:id)) do |style, index|
+      hash[style] = index
+    end
+    hash
+  end
 end
