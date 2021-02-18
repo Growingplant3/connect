332e2eec1563a491de6877e196e1574e7e530e5e
Yoshinori Shimoda <ys@platinum.local>
2021-02-18 15:46:41 +0900

    create/destroyアクションを追加

--- app/controllers/take_method_details_controller.rb
+++ app/controllers/take_method_details_controller.rb
@@ -1,8 +1,33 @@
 class TakeMethodDetailsController < ApplicationController
   def index
+    @take_method_details = TakeMethodDetail.all
   end
   
   def new
     @take_method_detail = TakeMethodDetail.new
+  end
+  
+  def create
+    @take_method_detail = TakeMethodDetail.new(take_method_detail_params)
+    if @take_method_detail.save
+      flash[:notice] = I18n.t('message.take_method_detail.create_success')
+      redirect_to take_method_details_path
+    else
+      flash.now[:notice] = I18n.t('message.take_method_detail.create_failure')
+      render :new
+    end
+  end
+
+  def destroy
+    @take_method_detail = TakeMethodDetail(params[:id])
+    @take_method_detail.destroy
+    flash[:notice] = I18n.t('message.take_method_detail.destroy_failure')
+    redirect_to take_method_details_path
+  end
+
+  private
+
+  def take_method_detail_params
+    params.require(:take_method_detail).permit(:style)
   end
 end
