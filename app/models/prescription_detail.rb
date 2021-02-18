50799d1237b54e2c5fd480eaa851a1aa0ccbfa27
Yoshinori Shimoda <ys@platinum.local>
2021-02-18 17:27:00 +0900

    親子関係の記述/ネストされたパラメーターの受け取り

--- app/models/prescription_detail.rb
+++ app/models/prescription_detail.rb
@@ -3,5 +3,8 @@ class PrescriptionDetail < ApplicationRecord
   has_many :medicine_record_relations, dependent: :destroy
   has_many :connect_medicines, through: :medicine_record_relations, source: :medicine
   accepts_nested_attributes_for :medicine_record_relations, allow_destroy: true
+  has_many :take_medicine_relations, dependent: :destroy
+  has_many :connect_methods, through: :take_medicine_relations, source: :take_method_detail
+  accepts_nested_attributes_for :take_medicine_relations, allow_destroy: true
   validates :prescription_days, :times, :daily_dose, :number_of_dose, presence: true
 end
