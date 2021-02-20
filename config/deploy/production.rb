a06739d95e14a3e7ee9b5cb8344e190a377d4f15
Yoshinori Shimoda <ys@platinum.local>
2021-02-20 19:56:25 +0900

    誤字修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server 'http://ec2-3-139-183-120.us-east-2.compute.amazonaws.com/', user: 'connect', roles: %w{app db web}
+server '3.1391.83.120', user: 'connect', roles: %w{app db web}
 set :ssh_options, keys: '/Users/yoshinori/.ssh/id_rsa'
