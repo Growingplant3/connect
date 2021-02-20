352ba71a3d58169434f4a2cb009e957dba4072d8
Yoshinori Shimoda <ys@platinum.local>
2021-02-21 00:21:55 +0900

    メールの設定変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   {
    user_name: ENV['SENDGRID_USERNAME'],
    password: ENV['SENDGRID_PASSWORD'],
-   domain: "heroku.com",
+   domain: "localhost",
    address: "smtp.sendgrid.net",
    port: 587,
    authentication: :plain,
