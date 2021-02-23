68f58028b67d81f5509e797f5ee9c49fcbab46a3
Yoshinori Shimoda <ys@platinum.local>
2021-02-23 23:30:13 +0900

    モデルのspec記述

--- spec/models/medicine_notebook_record_spec.rb
+++ spec/models/medicine_notebook_record_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe MedicineNotebookRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'お薬手帳記録作成機能', type: :model do
+  let(:new_user) { create(:new_user) }
+  let(:new_pharmacy) { create(:new_pharmacy) }
+  let(:medicine_notebook_record) { create(:medicine_notebook_record, user_id: new_user.id, pharmacy_id: new_pharmacy.id) }
+  
+  describe 'モデルの作成' do
+    context '最低限の新規登録' do
+      it 'ユーザーと薬局に紐づいた状態で、必須項目を満たせば医薬品を登録できる' do
+        expect(medicine_notebook_record).to be_valid
+      end
+    end
+  end
 end
