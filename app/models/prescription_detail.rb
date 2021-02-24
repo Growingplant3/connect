64a1fd7f5ba7b02ab451aa8753396002dd201c57
Yoshinori Shimoda <ys@platinum.local>
2021-02-24 12:02:54 +0900

    記述修正

--- app/models/prescription_detail.rb
+++ app/models/prescription_detail.rb
@@ -6,6 +6,5 @@ class PrescriptionDetail < ApplicationRecord
   has_many :take_medicine_relations, dependent: :destroy
   has_many :connect_methods, through: :take_medicine_relations, source: :take_method_detail
   accepts_nested_attributes_for :take_medicine_relations, allow_destroy: true
-  validates :prescription_days, :times, :daily_dose, presence: true
-  validates :number_of_dose, presence: true
+  validates :prescription_days, :times, :daily_dose, :number_of_dose, presence: true
 end
