6d65e9a0825081a4577bda55456df949f3d90368
Yoshinori Shimoda <ys@platinum.local>
2021-02-25 17:40:15 +0900

    お薬手帳記録のシステムテスト

--- spec/system/record_spec.rb
+++ spec/system/record_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe 'お薬手帳記録管理機能', type: :system do
   describe 'お薬手帳記録作成' do
     context '薬局でログイン後' do
       before { 
+        expect(MedicineNotebookRecord.count).to eq 0
         information_disclosure
         pharmacy_login(new_pharmacy)
-      }
-      it '必要事項を記入するとお薬手帳記録を作成できる' do
-        expect(MedicineNotebookRecord.count).to eq 0
         click_on 'ユーザー検索'
         click_on '新規登録ユーザー'
+      }
+      it '必要事項を記入するとお薬手帳記録を作成できる' do
         click_on 'お薬手帳記録を作成する'
         find('#medicine_notebook_record_date_of_issue').set('002020-02-20')
         find('#medicine_notebook_record_date_of_dispensing').set('002020-02-20')
@@ -25,6 +25,48 @@ RSpec.describe 'お薬手帳記録管理機能', type: :system do
         expect(MedicineNotebookRecord.count).to eq 1
         expect(current_path).to eq user_medicine_notebook_records_path(new_user)
         expect(page).to have_content 'お薬手帳記録を作成しました'
+      end
+    end
+
+    context 'お薬手帳が作成できない' do
+      before { 
+        expect(MedicineNotebookRecord.count).to eq 0
+        information_disclosure
+        pharmacy_login(new_pharmacy)
+        click_on 'ユーザー検索'
+        click_on '新規登録ユーザー'
+        click_on 'お薬手帳記録を作成する'
+      }
+      after {
+        expect(MedicineNotebookRecord.count).to eq 0
+        expect(page).to have_content '1 個のエラーがあるため、保存できません'
+      }
+      it '処方箋発行日が未入力だとお薬手帳記録を作成できない' do
+        find('#medicine_notebook_record_date_of_dispensing').set('002020-02-20')
+        fill_in 'medicine_notebook_record[medical_institution_name]', with: 'サンプル病院'
+        fill_in 'medicine_notebook_record[doctor_name]', with: 'サンプル医師'
+        click_on '登録する'
+      end
+
+      it '処方箋調剤日が未入力だとお薬手帳記録を作成できない' do
+        find('#medicine_notebook_record_date_of_issue').set('002020-02-20')
+        fill_in 'medicine_notebook_record[medical_institution_name]', with: 'サンプル病院'
+        fill_in 'medicine_notebook_record[doctor_name]', with: 'サンプル医師'
+        click_on '登録する'
+      end
+
+      it '医療機関名が未入力だとお薬手帳記録を作成できない' do
+        find('#medicine_notebook_record_date_of_issue').set('002020-02-20')
+        find('#medicine_notebook_record_date_of_dispensing').set('002020-02-20')
+        fill_in 'medicine_notebook_record[doctor_name]', with: 'サンプル医師'
+        click_on '登録する'
+      end
+
+      it '保険医氏名が未入力だと奥薬手帳記録を作成できない' do
+        find('#medicine_notebook_record_date_of_issue').set('002020-02-20')
+        find('#medicine_notebook_record_date_of_dispensing').set('002020-02-20')
+        fill_in 'medicine_notebook_record[medical_institution_name]', with: 'サンプル病院'
+        click_on '登録する'
       end
     end
   end
