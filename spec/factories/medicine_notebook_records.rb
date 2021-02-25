fbb270c17ffb70e85b5ea9cb24fbab599019e982
Yoshinori Shimoda <ys@platinum.local>
2021-02-25 14:57:27 +0900

    サンプルデータ修正

--- spec/factories/medicine_notebook_records.rb
+++ spec/factories/medicine_notebook_records.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :medicine_notebook_record do
-    date_of_issue { "2021-02-15" }
-    date_of_dispensing { "2021-02-15" }
-    medical_institution_name { "サンプル薬局" }
+    date_of_issue { "2021-02-20" }
+    date_of_dispensing { "2021-02-20" }
+    medical_institution_name { "サンプル病院" }
     doctor_name { "サンプル医師" }
     note { "特に無し" }
   end
